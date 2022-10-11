edd5e31c84d9f6f4824afe9be097c54da8d94083
zs <394596694@qq.com>
2022-10-11 09:08:59 +0000

    update src/main/java/com/zs/git/GitTest.java.
    Signed-off-by: zs <394596694@qq.com>

--- src/main/java/com/zs/git/GitTest.java
+++ src/main/java/com/zs/git/GitTest.java
@@ -13,6 +13,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("github test!");
         System.out.println("gitee test!");
-
+        System.out.println("gitee test222222222!");
     }
 }
