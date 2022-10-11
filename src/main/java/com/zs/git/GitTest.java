2397ae616ac68552e9e3090fb6531f9078c16559
zhangshuai <394596694@qq.com>
2022-10-11 16:18:54 +0800

    second commit

--- src/main/java/com/zs/git/GitTest.java
+++ src/main/java/com/zs/git/GitTest.java
@@ -6,5 +6,6 @@ package com.zs.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
