9b9129de158f0a53eac038730f03b44f0d9d9b66
zhangshuai3945 <115475461+zhangshuai3945@users.noreply.github.com>
2022-10-11 16:50:49 +0800

    Update GitTest.java

--- src/main/java/com/zs/git/GitTest.java
+++ src/main/java/com/zs/git/GitTest.java
@@ -11,5 +11,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("github test!");
+       
     }
 }
